59a3fe779f24b1e2473a2f97053f3934b8408624
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-11-06 09:01:30 +0600

    available food collection created and a get api created

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3500;
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.h7hr6qr.mongodb.net/?retryWrites=true&w=majority`;
@@ -15,19 +17,34 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
+app.get("/", async (req, res) => {
+  res.send("server is running");
+});
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    const availableFoodCollection = client
+      .db("community-food-service")
+      .collection("availableFoodCollection");
+
+    app.get("/availablefoods", async (req, res) => {
+      console.log("hitting");
+      const result = await availableFoodCollection.find().toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
